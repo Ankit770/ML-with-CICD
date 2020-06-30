95030a3e0a5e93489f1a0e2abe22eb343877189f
Ankit770 <abichpuriya@gmail.com>
2020-06-30 23:51:34 +0530

    Fixed syntax errors in publish_model script

--- scripts/publish_model.sh
+++ scripts/publish_model.sh
@@ -8,26 +8,26 @@ DIRS="$@"
 BASE_DIR=$(pwd)
 SETUP="setup.py"
 
-warn(){
+warn() {
     echo "$@" 1>&2
 }
 
-die(){
+die() {
     warn "$@"
     exit 1
 }
 
-build(){
+build() {
     DIR="${1/%\//}"
     echo "Checking directory $DIR"
     cd "$BASE_DIR/$DIR"
     [ ! -e $SETUP ] && warn "No $SETUP file, skipping" && return
-    PACKAGE_NAME=${python $SETUP --fullname}
+    PACKAGE_NAME=$(python $SETUP --fullname)
     echo "Package $PACKAGE_NAME"
     python "$SETUP" sdist bdist_wheel || die "Building package $PACKAGE_NAME failed"
     for X in $(ls dist)
     do
-        curl -F package=@"dist/$X" "$GEMFURY_URL" || die "uploading package $PACKAGE_NAME failed"
+        curl -F package=@"dist/$X" "$GEMFURY_URL" || die "uploading package $PACKAGE_NAME failed on file dist/$X"
     done
 }
 

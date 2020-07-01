ed520c7654760591bbc1b385a05e090b96c6bcb1
Ankit770 <abichpuriya@gmail.com>
2020-07-02 02:04:46 +0530

    fixed errors in test differential

--- packages/ml_api/tests/differential_tests/test_differential.py
+++ packages/ml_api/tests/differential_tests/test_differential.py
@@ -8,18 +8,18 @@ from api import config
 
 @pytest.mark.differential
 def test_model_prediction_differential(*,save_file='test_data_predictions.csv'):
-previous_model_df=pd.read_csv(f'{config.PACKAGE_ROOT}/{save_file}')
-previous_model_predictions=previous_model_df.prediction.values
-test_data=load_dataset(file_name=model_config.TESTING_DATA_FILE)
-multiple_test_json=test_data[99:600]
+    previous_model_df=pd.read_csv(f'{config.PACKAGE_ROOT}/{save_file}')
+    previous_model_predictions=previous_model_df.prediction.values
+    test_data=load_dataset(file_name=model_config.TESTING_DATA_FILE)
+    multiple_test_json=test_data[99:600]
 
-response=make_prediction(input_data=multiple_test_json)
-current_model_predictions=response.get('prediction')
+    response=make_prediction(input_data=multiple_test_json)
+    current_model_predictions=response.get('prediction')
 
-assert len(previous_model_predictions)==len(current_model_predictions)
+    assert len(previous_model_predictions)==len(current_model_predictions)
 
-for previous_value,current_value in zip(previous_model_predictions, current_model_predictions):
-    previous_value=previous_value.item()
-    current_value=current_value.item()
+    for previous_value,current_value in zip(previous_model_predictions, current_model_predictions):
+        previous_value=previous_value.item()
+        current_value=current_value.item()
 
-    assert math.isclose(previous_value,current_value,rel_tol=model_config.ACCEPTABLE_MODEL_DIFFERENCE)
+        assert math.isclose(previous_value,current_value,rel_tol=model_config.ACCEPTABLE_MODEL_DIFFERENCE)
